84fa06ae3660760a7e60af800b4c97b718d725f0
Mo0203 <97280087+Mo0203@users.noreply.github.com>
2022-03-30 16:38:34 +0200

    3n commit

--- controllers/organisationController.js
+++ controllers/organisationController.js
@@ -31,6 +31,43 @@ const createOrg = (req, res) => {
 };
 
 
+
+const updateOrg = async (req, res) => {
+
+    let id = req.body.id;
+    let name = req.body.name;
+
+    if (!ObjectId.isValid(id)) return res.status(400).json({ 'error': "L'ID spécifié pour la mise à jour de l'utilisateur est introuvable" });
+
+    Orga.findByIdAndUpdate(id, {
+        $set: {
+            name: name,
+        }
+
+    }, { new: true }, function (err, result) {
+        if (err) {
+            return res.status(400).json({ 'error': 'Echec de la mise a jour de l\'organisation' })
+        } else {
+            return res.status(200).json({ result });
+        }
+    });
+};
+
+const deleteOrg = async (req, res) => {
+    if (!ObjectId.isValid(req.body.id)) return res.status(400).json({ 'error': "L'ID spécifié pour la suppression de l'organisation est introuvable" });
+
+    Orga.findByIdAndDelete(
+        req.body.id,
+        (err, result) => {
+            if (!err) {
+                return res.status(200).json({ 'success': ' organisation supprimée avec succès' });
+            } else {
+                return res.status(500).json({ 'error': 'erreur serveur lors de la suppression de l\'organisation : ' + err });
+            }
+        }
+    )
+}
+
 function checkOrg(name, res) {
     if (name == "" || name == null) {
         return res.status(400).json({ 'error': 'Veuillez renseigner le nom de l\'organisation' });
@@ -38,7 +75,8 @@ function checkOrg(name, res) {
     if (!ORGA_REGEX.test(name)) {
         return res.status(400).json({ 'error': 'Nom d\'organisation invalide (pas de caractères spéciaux)' });
     }
-}
+};
+
 
 //On exporte nos fonctions
-module.exports = { getOrgs, createOrg };
+module.exports = { getOrgs, createOrg, updateOrg, deleteOrg };
--- routes/organisationRoutes.js
+++ routes/organisationRoutes.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const {
     createOrg,
     getOrgs,
-    // updateOrg,
-    // deleteOrg,
+    updateOrg,
+    deleteOrg,
 } = require('../controllers/organisationController.js');
 
 router.post('/createOrg', createOrg);
 router.get('/getOrgs', getOrgs);
-// router.post('/updateOrg', updateOrg);
-// router.post('/deleteOrg', deleteOrg);
+router.post('/updateOrg', updateOrg);
+router.post('/deleteOrg', deleteOrg);
 
 module.exports = router;
